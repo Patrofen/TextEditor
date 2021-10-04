d30bb14aa83116d7374b3c7b5c46754943fd49c1
Evgeniy_job <aee1983@gmail.com>
2021-10-04 18:22:08 +0300

    04.10.2021 continue
    + MessageService.cs +MainPresenter.cs

---
+++ MainPresenter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using TextEditor.BL;
+
+namespace TextEditor
+{
+    class MainPresenter
+    {
+        private readonly IMainForm _view;
+        private readonly IFileManager _manager;
+        private readonly IMessageService _messageService;
+
+        public MainPresenter (IMainForm view, IFileManager manager, IMessageService service)
+        {
+            _view = view;
+            _manager = manager;
+            _messageService = service;
+        }
+    }
+}
---
+++ TextEditor.BL/FileManager.cs
@@ -0,0 +1,45 @@
+﻿using System.IO;
+using System.Text;
+
+namespace TextEditor.BL
+{
+    public interface IFileManager
+    {
+        string GetContent(string filePath);
+        string GetContent(string filePath, Encoding encoding);
+        void SaveContent(string content, string filepath);
+        void SaveContent(string content, string filepath, Encoding encoding);
+        bool IsExist(string filePath);
+    }
+    public class FileManager:IFileManager
+    {
+        private readonly Encoding _defaultencoding = Encoding.GetEncoding(1251);
+        public bool IsExist(string filePath)
+        {
+            bool isExist = File.Exists(filePath);
+            return isExist;
+        }
+        public string GetContent(string filePath)
+        {
+            return GetContent(filePath, _defaultencoding);
+        }
+        public string GetContent(string filePath, Encoding encoding)
+        {
+            string content = File.ReadAllText(filePath, encoding);
+            return content;
+        }
+        public void SaveContent(string content, string filepath)
+        {
+            SaveContent(content, filepath, _defaultencoding);
+        }
+        public void SaveContent(string content, string filepath, Encoding encoding)
+        {
+            File.WriteAllText(filepath, content, encoding);
+        }
+        public int GetSymbolCount(string content)
+        {
+            int count = content.Length;
+            return count;
+        }
+    }
+}
